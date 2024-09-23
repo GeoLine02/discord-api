7e879579bd3d5060745b73daa6d165c1b12c2d03
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-09-23 15:14:25 +0400

    user CRUD

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
@@ -9,14 +10,35 @@ const {
 } = require("./src/config/index");
 const connetion = require("./src/postgresql");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // Replace with your frontend URL
+  res.header("Access-Control-Allow-Credentials", "true"); // Allow credentials
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE"); // Allowed methods
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+  );
+  next();
+});
 app.use(bodyParser.json());
+app.use(express.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 );
 
+app.use(cookieParser());
+const userRoutes = require("./src/routes/user.routes");
+
 connetion();
+
+app.use("/user", userRoutes);
 
 app.listen(port, () => console.log(`App is up and running on port ${port}`));
