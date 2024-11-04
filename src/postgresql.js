86731208b94e6623506c88847e40dce831955a36
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-11-04 23:17:58 +0400

    chore

--- src/postgresql.js
+++ src/postgresql.js
@@ -1,11 +1,37 @@
-const { Sequelize } = require("sequelize");
+// const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.INTERNAL_DATABASE_URL, {
+// const sequelize = new Sequelize(process.env.INTERNAL_DATABASE_URL, {
+//   dialect: "postgres",
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false, // Only if you're having SSL issues
+//     },
+//   },
+// });
+
+// const connection = async () => {
+//   try {
+//     await sequelize.authenticate();
+//     console.log("Connection has been established successfully.");
+//   } catch (error) {
+//     console.error("Unable to connect to the database:", error);
+//   }
+// };
+
+// module.exports = connection;
+
+const Sequelize = require("sequelize");
+
+const databaseUrl =
+  process.env.INTERNAL_DATABASE_URL || process.env.EXTERNAL_DATABASE_URL;
+
+const sequelize = new Sequelize(databaseUrl, {
   dialect: "postgres",
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false, // Only if you're having SSL issues
+      rejectUnauthorized: false,
     },
   },
 });
@@ -15,7 +41,11 @@ const connection = async () => {
     await sequelize.authenticate();
     console.log("Connection has been established successfully.");
   } catch (error) {
-    console.error("Unable to connect to the database:", error);
+    console.error(
+      "Unable to connect to the database:",
+      error.message,
+      error.stack
+    );
   }
 };
 
