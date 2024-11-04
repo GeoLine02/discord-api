2f431254e0f9d5f539718fb279ccb4e262e5355a
Nika Wuladze <126957977+GeoLine02@users.noreply.github.com>
2024-11-04 15:52:43 +0400

    chore: remove ssl config from production

--- src/sequelize/config/config.js
+++ src/sequelize/config/config.js
@@ -22,11 +22,5 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
   },
 };
